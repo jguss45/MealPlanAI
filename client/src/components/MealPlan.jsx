8e620aef3528accb533dd6766f01858e9eeb468a
Joe <jguss45@gmail.com>
2023-05-29 21:57:35 +0200

    meal plan output outline

--- client/src/components/MealPlan.jsx
+++ client/src/components/MealPlan.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
 
-const MealPlan = () => {
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import { fadeIn, textVariant, slideIn } from '../config/motion';
+import { styles } from '../styles';
+import state from '../store';
+import Loader from './Loader';
+import SubmitButton from './SubmitButton';
+
+
+const MealPlan = (text) => {
   return (
-    <div>MealPlan</div>
+    <motion.div
+      key={text}
+      variants={slideIn('left', 'tween', 0.2, 1)}
+      initial='hidden'
+      animate='show'
+      transition={{ duration: 0.3 }}
+      className='flex-[0.9] bg-black-100 p-8 rounded-2xl max-w-4xl mx-auto mt-4'
+    >
+      <p className={styles.sectionSubText}>Are you ready?</p>
+      <h3 className={styles.sectionHeadText}>HERE WE GO</h3>
+      <motion.p
+        variants={textVariant()}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        AI Meal Plan Goes Here
+        
+      </motion.p>
+    </motion.div>
   )
 }
 
--- client/src/config/motion.js
+++ client/src/config/motion.js
@@ -87,3 +87,45 @@ export const slideIn = (direction, type, delay, duration) => {
     },
   };
 };
+
+export const fadeIn = (direction, type, delay, duration) => {
+  //direction dictates direction of fadeIn: right means fade toward right
+  //delay useful for when using fadeIn on multiple elements and wanting fade to be 1 by 1. Using .map useful here and use the index to separate elements fading in
+  //duration is how long animation occurs for
+  return {
+    hidden: {
+      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const textVariant = (delay) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
