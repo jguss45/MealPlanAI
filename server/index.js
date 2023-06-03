0b35c69f88d0650d88288837c1f7dd7c6fd5b4b0
Joe <jguss45@gmail.com>
2023-06-03 20:01:29 +0200

    configured express backend

--- server/index.js
+++ server/index.js
@@ -4,4 +4,21 @@ import * as dotenv from 'dotenv';
     // in this case origin is mealGenius frontend, outside domain is openAI API
 import cors from 'cors'; 
 
-//import gpt.turbo from './routes/gpt.turbo.js';
+import gptRoutes from './routes/gpt.routes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors()); // enable CORS for all routes
+app.use(express.json({ limit: "50mb" })) // parse incoming requests with JSON and set limit of 50 mb on the request body size
+
+app.use("/api/v1/gpt", gptRoutes);
+
+//set up route for root URL of server
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello from GPT" })
+})
+
+
+//start server and listen on port 8080
+app.listen(8080, () => console.log("Listening on port 8080"));
--- server/routes/gpt.routes.js
+++ server/routes/gpt.routes.js
@@ -1,3 +1,54 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config(); // load env variables into process.env object
+
+// handle error in case api key isn't set
+if(!process.env.OPENAI_API_KEY) {
+    console.error("OPENAI API Key not found");
+    process.exit(1)
+}
+
+const router = express.Router();
+
+// create Configuration object to be used in OpenAIAPI object
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+}); 
+
+//create OpenAIAPI Object
+const openai = new OpenAIApi(config);
+
+// define api response to GET requests to root path
+//  respond with simple message
+router.route('/').get((req, res) => {
+    res.status(200).json({ message: "Hello from gpt-3.5-turbo routes" })
+});
+
+// define api response to POST requests to root path
+//  respond with generated image from DALL-E 
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+                "role": "user",
+                "content": { prompt }
+            }],
+            n: 1,
+        });
+
+        const text = response.data.choices[0].message.content;
+
+        res.status(200).json({ plan: text });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" })
+    }
+})
+
+export default router;
