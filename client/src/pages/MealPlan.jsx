9bb573eb0748afaaf9389ce5d708f5f6104d0beb
Joe <jguss45@gmail.com>
2023-06-20 23:41:27 +0200

    meal plan output formatting

--- client/src/pages/MealPlan.jsx
+++ client/src/pages/MealPlan.jsx
@@ -10,9 +10,7 @@ import formatMealPlanString from "../helpers/formatMealPlan";
 
 const MealPlan = () => {
   const snap = useSnapshot(state);
-  console.log(state.mealPlan)
   const formattedMealPlan = formatMealPlanString(snap.mealPlan);
-  console.log(formattedMealPlan)
 
   return (
     <AnimatePresence>
@@ -26,19 +24,32 @@ const MealPlan = () => {
               animate="show"
               transition={{ duration: 0.3 }}
               className="flex-[0.9] bg-black-100 p-8 rounded-2xl max-w-4xl mx-auto mt-4"
-              style={{ whiteSpace: "pre-wrap" }} //preserve the formatting we requested from the api
+              style={{ whiteSpace: "pre-wrap" }}
             >
               <p className={styles.sectionSubText}>Are you ready?</p>
               <h3 className={styles.sectionHeadText}>HERE WE GO</h3>
-              {/* {state.mealPlan} */}
-               {formattedMealPlan.map((dayString, index) => (
-              <motion.p
-                key={index}
-                variants={textVariant()}
-                className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-              >
-                {dayString}
-              </motion.p>
+
+              {formattedMealPlan.map((dayString, index) => (
+                <>
+                  <motion.p
+                    variants={textVariant()}
+                    className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+                    key={index}
+                  >
+                    {dayString}
+                  </motion.p>
+                  {/* Will add lines to separate each day of meal plan */}
+                  {index !== formattedMealPlan.length - 1 && (
+                    <motion.div
+                      variants={slideIn("left", "tween", 0.2, 1)}
+                      initial="hidden"
+                      animate="show"
+                      transition={{ duration: 0.3 }}
+                      className="border-b border-gray-300"
+                      key={`separator-${index}`}
+                    />
+                  )}
+                </>
               ))}
             </motion.div>
           </div>
